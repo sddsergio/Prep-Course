9146d9a2598fa30f43ca75318c3f97c617aa9c32
sddsergio <destefanosergio@hotmail.com>
2022-03-31 05:06:14 -0300

    homework

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -48,6 +48,15 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  if(idioma === 'aleman') {
+    return 'Guten Tag!';
+  } else if (idioma === 'mandarin') {
+    return 'Ni Hao!';
+  } else if (idioma === 'ingles') {
+    return 'Hello!';
+  } else {
+    return 'Hola!';
+  }
 }
 
 function colors(color) {
@@ -58,6 +67,7 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
+  
 }
 
 function esDiezOCinco(numero) {
